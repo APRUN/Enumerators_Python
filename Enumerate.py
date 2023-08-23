9a589f7ba2129f45795c62b886ad86358acb3d89
Khalil Ahmad <91117263+APRUN@users.noreply.github.com>
2023-08-23 05:00:56 -0700

    Add files via upload

---
+++ Enumerate.py
@@ -0,0 +1,26 @@
+a=[12,13,14,23,0,9,98,4,1]
+b={
+    "A":1,
+    "b":2,
+    "c":3
+}
+
+# for key,k in b.items():
+#     print(k)
+
+
+# index=0
+# for mark in a:
+#     print(mark)
+#     if(index==3):
+#         print("Chief!!!")
+#     index+=1
+    
+
+for index, mark in enumerate(a):
+    print(mark)
+    print("i: ",index)
+    # if(index==3):
+    #     print("Chief!!!")
+
+
